957e303f1a6d6821f1ff890462987bae564023c9
omer514 <aidegoomer5@gmail.com>
2025-12-18 12:00:58 +0100

    Modifications pour mise à jour Render

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
+    // Redirige la racine vers la liste des utilisateurs
     @GetMapping("/")
     public String home() {
-        return "redirect:/etudiants/list";
+        return "redirect:/utilisateurs/list";
     }
 }
